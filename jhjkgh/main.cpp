47f7ebd93e4720561b5df428ef50d51264aca0fa
Vivian <vivian0306@hotmail.com>
2017-06-06 16:24:43 -0500

    Caso 2 TASI TA
    Caso 1 - Asia, Publico, Servicios
    Caso 2 - Asia, Privado, Servicios

--- jhjkgh/main.cpp
+++ jhjkgh/main.cpp
@@ -24,5 +24,38 @@ int main(int argc, char** argv) {
 Se realizó una investigación con el objetivo de identificar la prioridad de los factores clave de éxito en la implementación del sistema de gestión de la seguridad de la información en la Organización Municipal de Irán. Los datos fueron recogidos mediante cuestionarios y los resultados fueron analizados con el software SPSS. El análisis obtenido sugiere que el apoyo de la alta dirección, la política de seguridad de la información y los programas de concienciación y capacitación son los factores de éxito más importantes en la implementación de sistemas de gestión de la seguridad de la información. En este estudio, el factor de los servicios de los asesores externos de seguridad de la información fue identificado como el menos importante.
 Kazemi, M., Khajouei, H., & Nasrabadi, H. (2012). Evaluation of information security management system success factors: Case study of Municipal organization. African Journal of Business Management, 6(14), 4982.
 
+
+
+Organización para el cuidado de
+la salud.Los
+incidentes de seguridad han afectado a las organizaciones de salud en todo el
+mundo y es necesario que estas aprendan de ellos para evitar que vuelvan a
+ocurrir. La Plantilla Genérica de Seguridad (P.G.S.) se ha desarrollado para
+mejorar las técnicas existentes utilizadas para compartir lecciones de
+incidentes de seguridad. Esta investigación pretende obtener una compresión
+sobre el contexto de la industria de la salud y la viabilidad de la P.G.S en
+ella.Un
+caso de estudio con una organización de salud representativa en China muestra
+que la organización tiene una cultura para proteger la información de seguridad
+desde que han comenzado a usar sistemas de información. Los gerentes apoyan la
+gestión de la seguridad y han tratado de mejorar su situación actual a través
+de algunas iniciativas. Los profesionales de la salud tienen una comprensión
+básica de la seguridad de la información. La organización anfitriona tiene un procedimiento
+de manejo de incidentes relativamente maduro, sin embargo, las lecciones
+aprendidas en los incidentes no se están tomando en cuenta para mejorar el
+sistema de gestión de seguridad de la información. También se identificaron fortalezas
+y debilidades de la P.G.S., así como los escenarios para aplicarla. 
+
+
+
+
+
+He, Y., Johnson, C., Lu, Y., & Lin, Y. (2014,
+May). Improving the information security management: An industrial study in the
+privacy of electronic patient records. In Computer-Based Medical Systems (CBMS),
+2014 IEEE 27th International Symposium on (pp. 525-526). IEEE.
+
+
+
 */
 
