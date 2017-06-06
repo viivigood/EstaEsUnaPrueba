5c53308478469eb44cf192d911d6b317d91c7b77
Vivian <vivian0306@hotmail.com>
2017-06-06 15:25:02 -0500

    Caso 1 TASI TA

--- jhjkgh/main.cpp
+++ jhjkgh/main.cpp
@@ -19,3 +19,10 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+/*
+•	Organización Municipal
+Se realizó una investigación con el objetivo de identificar la prioridad de los factores clave de éxito en la implementación del sistema de gestión de la seguridad de la información en la Organización Municipal de Irán. Los datos fueron recogidos mediante cuestionarios y los resultados fueron analizados con el software SPSS. El análisis obtenido sugiere que el apoyo de la alta dirección, la política de seguridad de la información y los programas de concienciación y capacitación son los factores de éxito más importantes en la implementación de sistemas de gestión de la seguridad de la información. En este estudio, el factor de los servicios de los asesores externos de seguridad de la información fue identificado como el menos importante.
+Kazemi, M., Khajouei, H., & Nasrabadi, H. (2012). Evaluation of information security management system success factors: Case study of Municipal organization. African Journal of Business Management, 6(14), 4982.
+
+*/
+
